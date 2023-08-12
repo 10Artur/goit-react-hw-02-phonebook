5ddd463f56f79ef2a289513b164d5f3555c74767
10Artur <artyr11012002@gmail.com>
2023-08-13 02:09:15 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactsForm/ContactsForm';
+import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+import { GlobalStyle } from './GlobalStyle';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  addContact = newContact => {
+    if (this.state.contacts.find(contact => contact.name === newContact.name)) {
+      return alert(`${newContact.name} is already in contacts!`);
+    }
+
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, newContact],
+      };
+    });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId
+        ),
+      };
+    });
+  };
+
+  changeContactFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  render() {
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onAddContact={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter
+          onChangeContactFilter={this.changeContactFilter}
+          value={this.state.filter}
+        />
+        <ContactsList
+          filteredContacts={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+        <GlobalStyle />
+      </div>
+    );
+  }
+}
